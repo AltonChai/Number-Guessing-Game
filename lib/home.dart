eaa395bb04f56c90895394e9a0e7b825a6d2336e
NootNoot <101384541+AltonChai@users.noreply.github.com>
2024-07-25 04:26:48 +0800

    added ui
    9 button and top 4 button

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("data"),
+            const Text(
+              "Title",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Row(
+              //the top 4 circle
+
+              children: List.generate(
+                4,
+                (index) => Container(
+                  margin: EdgeInsets.only(right: 10),
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                      border: Border.all(color: Colors.white)),
+                ),
+              ),
+            ),
+            Expanded(
+              //what is this
+              child: GridView.count(
+                crossAxisCount: 3,
+                mainAxisSpacing: 50,
+                crossAxisSpacing: 50,
+                children: List.generate(
+                  10,
+                  (index) => CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      index < 9 ? '${index + 1}' : '0',
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
